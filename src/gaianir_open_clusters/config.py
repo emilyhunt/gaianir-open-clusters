38be41c23071852e04510d97e2e9c16e046c6f4d
emilyhunt <emily.hunt.physics@gmail.com>
2026-05-28 17:29:44 +0200

    Test and install synthpop

---
+++ src/gaianir_open_clusters/config.py
@@ -0,0 +1,15 @@
+import gaianir_open_clusters
+from pathlib import Path
+
+
+INSTALLATION_DIRECTORY = Path(gaianir_open_clusters.__path__[0]).parent.parent
+DATA_DIRECTORY = INSTALLATION_DIRECTORY / "data"
+SYNTHPOP_CONFIG_DIRECTORY = INSTALLATION_DIRECTORY / "synthpop_configs"
+RESULTS_DIRECTORY = INSTALLATION_DIRECTORY / "results"
+
+
+# Synthpop things
+SYNTHPOP_SPECIFIC_CONFIG = str(SYNTHPOP_CONFIG_DIRECTORY / "gaia_nir.synthpop_conf")
+SYNTHPOP_DEFAULT_CONFIG = str(
+    SYNTHPOP_CONFIG_DIRECTORY / "huston2025_defaults.synthpop_conf"
+)
